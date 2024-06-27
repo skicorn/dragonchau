9d8b9e6d8e0d933fa30e3f79bd46519add49551f
quagthanh <dinhquangthanh11@gmail.com>
2024-06-27 13:20:48 +0700

    cc

---
+++ dragonchau/Controllers/StaffsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using dragonchau.Models;
+
+namespace dragonchau.Controllers
+{
+    public class StaffsController : Controller
+    {
+        private dragonchauEntities db = new dragonchauEntities();
+
+        // GET: Staffs
+        public ActionResult Index()
+        {
+            var staffs = db.Staffs.Include(s => s.Account).Include(s => s.Role);
+            return View(staffs.ToList());
+        }
+
+        // GET: Staffs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Staff staff = db.Staffs.Find(id);
+            if (staff == null)
+            {
+                return HttpNotFound();
+            }
+            return View(staff);
+        }
+
+        // GET: Staffs/Create
+        public ActionResult Create()
+        {
+            ViewBag.StaffID = new SelectList(db.Accounts, "StaffID", "StaffPassword");
+            ViewBag.StaffRole = new SelectList(db.Roles, "RoleID", "RoleName");
+            return View();
+        }
+
+        // POST: Staffs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "StaffID,StaffName,StaffPhone,StaffEmail,StaffSalary,StaffIDnum,StaffDateCreate,StaffRole")] Staff staff)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Staffs.Add(staff);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.StaffID = new SelectList(db.Accounts, "StaffID", "StaffPassword", staff.StaffID);
+            ViewBag.StaffRole = new SelectList(db.Roles, "RoleID", "RoleName", staff.StaffRole);
+            return View(staff);
+        }
+
+        // GET: Staffs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Staff staff = db.Staffs.Find(id);
+            if (staff == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.StaffID = new SelectList(db.Accounts, "StaffID", "StaffPassword", staff.StaffID);
+            ViewBag.StaffRole = new SelectList(db.Roles, "RoleID", "RoleName", staff.StaffRole);
+            return View(staff);
+        }
+
+        // POST: Staffs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "StaffID,StaffName,StaffPhone,StaffEmail,StaffSalary,StaffIDnum,StaffDateCreate,StaffRole")] Staff staff)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(staff).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.StaffID = new SelectList(db.Accounts, "StaffID", "StaffPassword", staff.StaffID);
+            ViewBag.StaffRole = new SelectList(db.Roles, "RoleID", "RoleName", staff.StaffRole);
+            return View(staff);
+        }
+
+        // GET: Staffs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Staff staff = db.Staffs.Find(id);
+            if (staff == null)
+            {
+                return HttpNotFound();
+            }
+            return View(staff);
+        }
+
+        // POST: Staffs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Staff staff = db.Staffs.Find(id);
+            db.Staffs.Remove(staff);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
